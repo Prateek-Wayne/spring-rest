cd01f933b02c1f496c00d28f3e6bfc08b69eadbb
Prateek-Wayne <lordprateekverma@gmail.com>
2025-06-29 19:37:04 +0530

    initialize Spring CRUD application with Maven setup and basic structure
    Signed-off-by: Prateek-Wayne <lordprateekverma@gmail.com>

--- src/main/java/org/prateek/SpringCrudApplication.java
+++ src/main/java/org/prateek/SpringCrudApplication.java
@@ -2,12 +2,18 @@ package org.prateek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCrudApplication.class, args);
     }
-
+    @GetMapping
+    public String hello() {
+        return "Hello, World 🍃";
+    }
 }
